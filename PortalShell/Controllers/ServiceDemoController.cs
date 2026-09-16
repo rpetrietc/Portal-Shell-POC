f173816fe22a51b864fd7693fb4521174d00f726
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2026-09-16 13:35:16 -0400

    Add embedded service demonstration

---
+++ PortalShell/Controllers/ServiceDemoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortalShell.Controllers;
+
+public class ServiceDemoController : Controller
+{
+    public IActionResult Embedded()
+    {
+        return View();
+    }
+}
