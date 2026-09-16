c3cc60cf1c9b31e4d22f52c9ddd6c872025008f4
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2026-09-16 13:14:03 -0400

    Connect home controller to Government of Canada web template

--- PortalShell/Controllers/HomeController.cs
+++ PortalShell/Controllers/HomeController.cs
@@ -1,18 +1,26 @@
+using GoC.WebTemplate.Components.Core.Services;
+using GoC.WebTemplate.CoreMVC.Controllers;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using PortalShell.Models;
 
 namespace PortalShell.Controllers;
 
-public class HomeController : Controller
+public class HomeController : WebTemplateBaseController
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(
+        ModelAccessor modelAccessor,
+        ILogger<HomeController> logger)
+        : base(modelAccessor)
     {
         _logger = logger;
-    }
 
+        // Sets the application name displayed by the
+        // Government of Canada Web Template.
+        WebTemplateModel.HeaderTitle = "Portal Shell";
+    }
     public IActionResult Index()
     {
         return View();
