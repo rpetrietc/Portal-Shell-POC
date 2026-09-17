f053e4f6c8392c112045823daa4f4b0a73cb720c
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2026-09-17 15:36:15 -0400

    code for embedding Digital Personnel Licence in an iframe

---
+++ PortalShell/Controllers/DplDemoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortalShell.Controllers;
+
+public class DplDemoController : Controller
+{
+    public IActionResult Embedded()
+    {
+        return View();
+    }
+}
