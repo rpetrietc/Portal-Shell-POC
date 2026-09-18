0e6bcdede8e23bda057742c8cfda0f2bcbf8b411
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2026-09-18 13:22:54 -0400

    Configure Canada web template services

--- ConnectedService/Program.cs
+++ ConnectedService/Program.cs
@@ -1,7 +1,16 @@
+using GoC.WebTemplate.Components.Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add MVC services.
 builder.Services.AddControllersWithViews();
+
+// Required by the Government of Canada Web Template.
+// ModelAccessor provides the template model used by controllers and views.
+builder.Services.AddModelAccessor();
+
+// Configures English/French request localization used by the template.
+builder.Services.ConfigureGoCTemplateRequestLocalization();
 
 var app = builder.Build();
 
@@ -9,12 +18,14 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
+
+// Enable the request localization configured above.
+app.UseRequestLocalization();
 
 app.UseAuthorization();
 
@@ -24,6 +35,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
