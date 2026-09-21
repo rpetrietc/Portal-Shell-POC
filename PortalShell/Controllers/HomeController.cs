8458afbe68f9c9ee453e7a57cc2c29f2ce12aeb2
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2026-09-21 13:50:40 -0400

    Load portal links from SQLite

--- PortalShell/Controllers/HomeController.cs
+++ PortalShell/Controllers/HomeController.cs
@@ -1,29 +1,43 @@
 using GoC.WebTemplate.Components.Core.Services;
 using GoC.WebTemplate.CoreMVC.Controllers;
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PortalShell.Data;
 using PortalShell.Models;
+using System.Diagnostics;
 
 namespace PortalShell.Controllers;
 
 public class HomeController : WebTemplateBaseController
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly PortalShellContext _portalShellContext;
 
     public HomeController(
         ModelAccessor modelAccessor,
-        ILogger<HomeController> logger)
+        ILogger<HomeController> logger,
+        PortalShellContext portalShellContext)
         : base(modelAccessor)
     {
         _logger = logger;
+        _portalShellContext = portalShellContext;
 
         // Sets the application name displayed by the
         // Government of Canada Web Template.
-        WebTemplateModel.HeaderTitle = "Portal Shell";
+        WebTemplateModel.HeaderTitle = "CivAv Portal Proof Of Concept";
     }
-    public IActionResult Index()
+
+    public async Task<IActionResult> Index()
     {
-        return View();
+        // Load only enabled links from the database.
+        // DisplayOrder controls the order they will appear on the page.
+        var portalLinks = await _portalShellContext.PortalLinks
+            .Where(link => link.IsEnabled)
+            .OrderBy(link => link.DisplayOrder)
+            .ToListAsync();
+
+        // Pass the database-generated links to the Home view.
+        return View(portalLinks);
     }
 
     public IActionResult Privacy()
@@ -31,9 +45,17 @@ public class HomeController : WebTemplateBaseController
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [ResponseCache(
+        Duration = 0,
+        Location = ResponseCacheLocation.None,
+        NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(new ErrorViewModel
+        {
+            RequestId =
+                Activity.Current?.Id ??
+                HttpContext.TraceIdentifier
+        });
     }
 }
