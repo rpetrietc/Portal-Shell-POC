40f55fb8e0f8f20712180fc23c1877441e638185
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2026-09-21 13:53:42 -0400

    Prepare database links for shared navigation

--- PortalShell/Controllers/HomeController.cs
+++ PortalShell/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
 using GoC.WebTemplate.Components.Core.Services;
 using GoC.WebTemplate.CoreMVC.Controllers;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using PortalShell.Data;
 using PortalShell.Models;
 using System.Diagnostics;
 
@@ -11,33 +9,22 @@ namespace PortalShell.Controllers;
 public class HomeController : WebTemplateBaseController
 {
     private readonly ILogger<HomeController> _logger;
-    private readonly PortalShellContext _portalShellContext;
 
     public HomeController(
         ModelAccessor modelAccessor,
-        ILogger<HomeController> logger,
-        PortalShellContext portalShellContext)
+        ILogger<HomeController> logger)
         : base(modelAccessor)
     {
         _logger = logger;
-        _portalShellContext = portalShellContext;
 
         // Sets the application name displayed by the
         // Government of Canada Web Template.
         WebTemplateModel.HeaderTitle = "CivAv Portal Proof Of Concept";
     }
 
-    public async Task<IActionResult> Index()
+    public IActionResult Index()
     {
-        // Load only enabled links from the database.
-        // DisplayOrder controls the order they will appear on the page.
-        var portalLinks = await _portalShellContext.PortalLinks
-            .Where(link => link.IsEnabled)
-            .OrderBy(link => link.DisplayOrder)
-            .ToListAsync();
-
-        // Pass the database-generated links to the Home view.
-        return View(portalLinks);
+        return View();
     }
 
     public IActionResult Privacy()
