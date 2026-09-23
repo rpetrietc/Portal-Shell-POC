6f5f8f2909524fb6bfc825eb9df25f12110dbe0e
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2026-09-23 09:46:21 -0400

    Add roles administration list

---
+++ PortalAdmin/Controllers/RolesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PortalData.Data;
+
+namespace PortalAdmin.Controllers;
+
+public class RolesController : Controller
+{
+    private readonly PortalShellContext _portalShellContext;
+
+    public RolesController(PortalShellContext portalShellContext)
+    {
+        _portalShellContext = portalShellContext;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        // Load all roles and their assigned portal links
+        // for display in the administration application.
+        var roles = await _portalShellContext.Roles
+            .AsNoTracking()
+            .Include(role => role.PortalLinks)
+            .OrderBy(role => role.Name)
+            .ToListAsync();
+
+        return View(roles);
+    }
+}
